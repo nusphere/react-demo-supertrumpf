2a36eb60c3044b9e5fe8bd8fad423475a1233fca
Sebastian Hopfe <s.hopfe@me.com>
2022-10-02 13:15:50 +0200

    allow 2 space for tab indent's

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
-    env: {
-        browser: true,
-    },
-    extends: [
-        'airbnb',
-    ],
-    rules: {
-        // Indent with 4 spaces
-        indent: ['error', 4],
-
-        // Indent JSX with 4 spaces
-        'react/jsx-indent': ['error', 4],
-
-        // Indent props with 4 spaces
-        'react/jsx-indent-props': ['error', 4],
-    },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb',
+  ],
 };
--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css';
 import Card from './Card';
 
 export default function App() {
-    return (
-        <div>
-            <h1>Supertrumpf</h1>
-            <Card />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Supertrumpf</h1>
+      <Card />
+    </div>
+  );
 }
