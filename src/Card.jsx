4e38a59a1a22e0a3daae4d609cf618dbc90989ca
Sebastian Hopfe <s.hopfe@me.com>
2022-10-02 12:27:07 +0200

    add placeholder image

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
   return (
     <div className="card">
       <h1>Elefant</h1>
-      <img alt="Elefant" height="200" width="200" src={`${process.env.PUBLIC_URL}/placeholder.png`} />
+      <img alt="Elefant" height="200" width="200" src={`${process.env.PUBLIC_URL}/placeholder.jpg`} />
       <table>
         <tbody>
           <tr>
