5d0fd4eff3b4edce6a39169d5490866437508aee
Sebastian Hopfe <s.hopfe@me.com>
2022-10-02 13:08:02 +0200

    allow 4 space for tab indent's

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
+    env: {
+        browser: true,
     },
-    "extends": [
-        "airbnb",
+    extends: [
+        'airbnb',
     ],
-}
+    rules: {
+        // Indent with 4 spaces
+        indent: ['error', 4],
+
+        // Indent JSX with 4 spaces
+        'react/jsx-indent': ['error', 4],
+
+        // Indent props with 4 spaces
+        'react/jsx-indent-props': ['error', 4],
+    },
+};
---
+++ src/Animal.js
@@ -0,0 +1,11 @@
+export default class Animal {
+    constructor(name, image, size, weight, age, offspring, speed) {
+        this.name = name;
+        this.image = image;
+        this.size = size;
+        this.weight = weight;
+        this.age = age;
+        this.offspring = offspring;
+        this.speed = speed;
+    }
+}
