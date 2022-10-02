40e977fdba85b804b38c07f9ed40630393fd7e64
Sebastian Hopfe <s.hopfe@me.com>
2022-10-02 11:53:18 +0200

    use card componet in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import Card from './Card';
 
-export default class App extends Component {
-  render() {
-    return <h1>Supertrumpf</h1>;
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>Supertrumpf</h1>
+      <Card />
+    </div>
+  );
 }
