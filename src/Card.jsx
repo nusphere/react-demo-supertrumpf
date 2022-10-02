a662bee189e640ea268dfb2ad1b94e2155bdd0a5
Sebastian Hopfe <sebastian.hopfe@milchundzucker.de>
2022-10-02 11:42:32 +0200

    add first card component

---
+++ src/Card.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+export default function Card() {
+  return (
+    <div className="card">
+      <h1>Elefant</h1>
+      <img alt="Elefant" height="200" width="200" src={`${process.env.PUBLIC_URL}/placeholder.png`} />
+      <table>
+        <tbody>
+          <tr>
+            <td>Größe</td>
+            <td>3.3 m</td>
+          </tr>
+          <tr>
+            <td>Gewicht</td>
+            <td>6000 kg</td>
+          </tr>
+          <tr>
+            <td>Alter</td>
+            <td>70 Jahre</td>
+          </tr>
+          <tr>
+            <td>Nachkommen</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>Geschwindigkeit</td>
+            <td>40 km/h</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
